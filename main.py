fb9d51d2766e8fdd7d0c9706991202207a4ce68f
Blake-Schmitz <blakenschmitz@gmail.com>
2026-06-04 15:23:44 -0500

    Outliers and Threshold requests functional

---
+++ main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import outliers
+import uvicorn
+
+
+URL = '127.0.0.1'
+PORT = 5000
+
+app = FastAPI()
+
+class OutliersRequest(BaseModel):
+    data: list
+
+
+@app.post('/max/')
+def maximum(request: OutliersRequest):
+    result = outliers.maximum(request.data)
+    return {'result': result['result'], 'index': result['index']}
+
+@app.post('/min/')
+def minimum(request: OutliersRequest):
+    result = outliers.minimum(request.data)
+    return {'result': result['result'], 'index': result['index']}
+
+@app.post('/long/')
+def long(request: OutliersRequest):
+    result = outliers.long(request.data)
+    return {'result': result['result'], 'index': result['index']}
+
+@app.post('/short/')
+def short(request: OutliersRequest):
+    result = outliers.short(request.data)
+    return {'result': result['result'], 'index': result['index']}
+
+@app.post('/threshold/')
+def threshold(request: OutliersRequest, threshold: float=0):
+    result = outliers.short(request.data, threshold)
+    return {'result': result['result'], 'percent': result['percent']}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host = URL, port = PORT)
